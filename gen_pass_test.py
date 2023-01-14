3402b132065261f29f714c0dab6f422e663a356e
Benjamin Johnson <50123967+bnjn@users.noreply.github.com>
2023-01-14 15:39:10 +0000

    fixed different password test to catch edge case. added test for unique chars.

--- gen_pass_test.py
+++ gen_pass_test.py
@@ -13,6 +13,10 @@ class TestGenPass(unittest.TestCase):
     expected = 10
     self.assertEqual(len(actual), expected)
 
+  def test_raises_exception_if_length_under_two(self):
+    with self.assertRaises(ValueError):
+      gen_pass.generate_password(1)
+
   def test_throw_typeerror_on_invalid_input(self):
     with self.assertRaises(TypeError):
       gen_pass.generate_password('potato')
@@ -20,8 +24,18 @@ class TestGenPass(unittest.TestCase):
   def test_returns_different_passwords(self):
     first_gen = gen_pass.generate_password(6)
     second_gen = gen_pass.generate_password(6)
-
+    loop_number = 0
+    while first_gen == second_gen:
+      if loop_number > 100:
+        break
+      second_gen = gen_pass.generate_password(6)
+      loop_number += 1
     self.assertNotEqual(first_gen, second_gen)
+
+  def test_does_not_return_same_chars(self):
+    actual = gen_pass.generate_password(12)
+    different_chars = len(set(list(actual))) > 1
+    self.assertTrue(different_chars)
 
 if __name__ == '__main__': 
     unittest.main()
