9daff677d1ffe93c03090861a26cf111750dd9c0
Benjamin Johnson <50123967+bnjn@users.noreply.github.com>
2023-01-14 16:28:27 +0000

    passes tests for length over 100 and contains numbers.

--- gen_pass.py
+++ gen_pass.py
@@ -4,10 +4,14 @@ import random
 def generate_password(length):
   if type(length) != int:
     raise TypeError('Integers are the only valid input.')
-  if length < 2:
+  elif length < 2:
     raise ValueError('Length must be two or more.')
+  elif length > 100:
+    raise ValueError('Length must be less than a hundred.')
   letters = string.ascii_letters
+  numbers = ''.join(str(i) for i in list((range(0, 10))))  
+  chars = letters + numbers
   password = ''
   for i in range(length):
-    password += random.choice(letters)
+    password += random.choice(chars)
   return password
